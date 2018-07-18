f55b97756f28b8fc3c0f539053986cfa09c6f33e
nikilic <nikolailic02@gmail.com>
2018-07-18 18:02:31 +0100

    Ispravka test.py

--- Tests/test.py
+++ Tests/test.py
@@ -15,6 +15,7 @@ def category_get():
 def category_post():
     payload = {'name': 'example'}
     r = requests.post('http://0.0.0.0:8090/api/Category', data=json.dumps(payload))
+    result = json.loads(r.text.decode('utf-8'))
     id = result["data"]["id"]
     payload = {'id': id, 'name': 'example'}
     rdel = requests.delete('http://0.0.0.0:8090/api/Category', data=json.dumps(payload))
@@ -32,6 +33,7 @@ def category_put():
 def category_delete():
     payload = {'name': 'example'}
     r = requests.post('http://0.0.0.0:8090/api/Category', data=json.dumps(payload))
+    result = json.loads(r.text.decode('utf-8'))
     id = result["data"]["id"]
     payload = {'id': id, 'name': 'example'}
     rdel = requests.delete('http://0.0.0.0:8090/api/Category', data=json.dumps(payload))
@@ -47,66 +49,3 @@ def comment_post():
 def comment_get():
     r = requests.get('http://0.0.0.0:8090/api/Comment')
     return r.status_code
-
-
-# api/Hello GET
-print "Hello World:"
-r = requests.get('http://0.0.0.0:8090/api/Hello')
-if r.status_code == 200:
-    print "OK"
-else:
-    print r.status_code
-
-# api/Category GET
-print "Category GET:"
-r = requests.get('http://0.0.0.0:8090/api/Category')
-if r.status_code == 200:
-    print "OK"
-else:
-    print r.status_code
-
-# api/Category POST
-print "Category POST:"
-payload = {'name': 'example'}
-r = requests.post('http://0.0.0.0:8090/api/Category', data=json.dumps(payload))
-result = json.loads(r.text.decode('utf-8'))
-id = result["data"]["id"]
-if r.status_code == 201:
-    print "OK"
-else:
-    print r.status_code
-
-# api/Category PUT
-print "Category PUT:"
-payload = {'id': id, 'name': 'reserved'}
-r = requests.put('http://0.0.0.0:8090/api/Category', data=json.dumps(payload))
-if r.status_code == 204:
-    print "OK"
-else:
-    print r.status_code
-
-# api/Comment POST
-print "Comment POST:"
-payload = {'category_id': id, 'comment': 'COMMENT'}
-r = requests.post('http://0.0.0.0:8090/api/Comment', data=json.dumps(payload))
-if r.status_code == 201:
-    print "OK"
-else:
-    print r.status_code
-
-# api/Comment GET
-print "Comment GET:"
-r = requests.get('http://0.0.0.0:8090/api/Comment')
-if r.status_code == 200:
-    print "OK"
-else:
-    print r.status_code
-
-# api/Category DELETE
-print "Category DELETE:"
-payload = {'id': id, 'name': 'reserved'}
-r = requests.delete('http://0.0.0.0:8090/api/Category', data=json.dumps(payload))
-if r.status_code == 204:
-    print "OK"
-else:
-    print r.status_code
